4b1e382facd94eb78b1d5af7b07de5fb400fa4a0
PitesFan <valeriacroitoru03@gmail.com>
2026-01-17 11:17:59 +0200

    add files

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Steam {
+
+};
+
+
+/*
+class Car {
+public:
+    string make;
+    string model;
+
+    Car(string carMake, string carModel) {
+        make = carMake;
+        model = carModel;
+        fuelLevel = 100;
+    }
+
+    void displayInfo() {
+        cout << "Marca: "<<make<<", Model: "<<model << endl;
+        cout<<"Nivel combustibil: "<<fuelLevel<<endl;
+    }
+
+    void drive() {
+        if (fuelLevel > 0) {
+            fuelLevel -=10;
+            cout<<"Conduci la 60 km/h. Nivel combustibil: "<<fuelLevel<<endl;
+        } else {
+            cout<<"Combustibil epuizat! Trebuie să alimentezi."<<endl;
+        }
+    }
+
+private:
+    int fuelLevel;
+
+};
+
+int main(){
+
+    Car myCar("Dacia", "Sandero");
+    myCar.displayInfo();
+    myCar.drive();
+    myCar.displayInfo();
+
+return 0;
+}
+*/
